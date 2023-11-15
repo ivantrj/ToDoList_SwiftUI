49a8b981c36014044f627028ae1c008cd8bf31eb
ivan <ivantrajanovski@gmail.com>
2023-11-15 16:34:39 +0100

    set up firebase

--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -5,10 +5,15 @@
 //  Created by ivan ruwido  on 15.11.23.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct ToDoListApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
